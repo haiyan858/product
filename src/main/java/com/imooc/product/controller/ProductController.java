a39fe9625e30870229a10299c230f19f9da6e05a
cuihaiyan <haiyan.cui@zebra-c.com>
2019-10-03 23:12:54 +0800

    构建项目

---
+++ src/main/java/com/imooc/product/controller/ProductController.java
@@ -0,0 +1,18 @@
+package com.imooc.product.controller;
+
+/**
+ * @Author cuihaiyan
+ * @Create_Time 2019-10-03 23:09
+ */
+public class ProductController {
+
+    /**
+     * 1.查询所有在架的商品
+     * 2.获取类目type列表
+     * 3.查询类目
+     * 4.构造数据
+     */
+    public void list(){
+
+    }
+}
