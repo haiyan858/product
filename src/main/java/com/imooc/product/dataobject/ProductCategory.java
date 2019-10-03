e251cf1c4f1a7b4ce1627c2a64e9f68a85e9dcb0
cuihaiyan <haiyan.cui@zebra-c.com>
2019-10-03 23:19:11 +0800

    数据库表的实体类

---
+++ src/main/java/com/imooc/product/dataobject/ProductCategory.java
@@ -0,0 +1,30 @@
+package com.imooc.product.dataobject;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * @Author cuihaiyan
+ * @Create_Time 2019-10-03 23:19
+ */
+@Data
+@Entity
+public class ProductCategory {
+    @Id
+    @GeneratedValue
+    private Integer categoryId;
+
+    /** 类目名字. */
+    private String categoryName;
+
+    /** 类目编号. */
+    private Integer categoryType;
+
+    private Date createTime;
+
+    private Date updateTime;
+}
